597b89ae9d8c08690a413aa2403013d70c665e43
himanshuDev <himanshu.nikhil.gupta@gmail.com>
2023-07-19 20:39:37 +0530

    Update longestWord.js
    Final Change

--- longestWord.js
+++ longestWord.js
@@ -1,6 +1,10 @@
 function findLongestWord(str) {
-   let strSplit = str.split(' ');
-   let longestWord = 0;
+  if(!str){
+	  return "Please Pass String";
+  }else{
+  let strSplit = str.split(' ');
+  let longestWord = 0;
+  //-----------Here is Array Loop to check Lenght--------------------------//	
   for(let i = 0; i < strSplit.length; i++){
     if(strSplit[i].length > longestWord){ 
 	longestWord = strSplit[i].length; 
@@ -8,7 +12,7 @@ function findLongestWord(str) {
   }
  
   return longestWord; 
+ }
 }
-
 let newVal=findLongestWord("My name is ankit jain & i'm from morena morema");
 console.log(newVal);
