8f6eb023468d60273024eaa9315b17a981831f53
himanshuDev <himanshu.nikhil.gupta@gmail.com>
2025-09-07 17:45:13 +0530

    Update simple.js

--- Start/simple.js
+++ Start/simple.js
@@ -27,6 +27,60 @@ for (let i = 0; i <= n; i++) {
     console.log(row);
 }
 
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+// 1 2 3 4 5
+
+let n = 5;
+for (let i = 1; i <= n; i++) {
+    let row = "1";
+    for (let j = 0; j <i-1; j++) {
+        row += j+2;
+    }
+    console.log(row);
+}
+
+// 1
+// 2 2
+// 3 3 3
+// 4 4 4 4
+
+let n = 4;
+for (let i = 0; i < n; i++) {
+    let row = "";
+    for (let j = 0; j <= i; j++) {
+        row += (i + 1);
+    }
+    console.log(row);
+}
+
+or
+
+let n = 4;
+for (let i = 0; i <= n; i++) {
+    let row = "";
+    for (let j = 1; j <= i; j++) {
+        row += i;
+    }
+    console.log(row);
+}
+
+// 1 2 3 4
+// 1 2 3
+// 1 2
+// 1
+
+let n = 4;
+for (let i = 0; i < n; i++) {
+    let row = "";
+    for (let j = 0; j < n - i; j++) {
+        row += (j + 1);
+    }
+    console.log(row);
+}
+
 
 
 
