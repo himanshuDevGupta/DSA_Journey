9a371ced188b5b2c22a707e973d4ed47bb189b21
himanshuDev <himanshu.nikhil.gupta@gmail.com>
2025-09-08 13:27:20 +0530

    Update simple.js

--- Start/simple.js
+++ Start/simple.js
@@ -126,6 +126,21 @@ for (let i = 0; i <n; i++) {
     console.log(row);
 }
 
+// 1
+// 1 0
+// 1 0 1
+// 1 0 1 0
+let n = 4;
+for (let i = 0; i < n; i++) {
+    let row = "";
+    let toggle = 1;
+    for (let j = 0; j < i + 1; j++) {
+        row += toggle;
+        toggle = toggle === 1 ? 0 : 1;
+    }
+    console.log(row);
+}
+
 
 
 
