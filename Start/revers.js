abd22929110f795650c0ec73453f685d01c565ab
himanshuDev <himanshu.nikhil.gupta@gmail.com>
2025-09-09 11:47:18 +0530

    Update revers.js

--- Start/revers.js
+++ Start/revers.js
@@ -1,1 +1,13 @@
+var reverse = function (x) {
+    let rev = 0;
+    while (x > 0) {
+        let rem = x % 10;
+        rev = rev * 10 + rem;
+        x = Math.floor(x / 10);
 
+        
+    }
+    return rev;
+};
+
+console.log(reverse(123)); // true
