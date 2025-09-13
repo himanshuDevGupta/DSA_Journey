8135c08815e5d53c9f27f9f59bd64b3d4e065cc6
himanshuDev <himanshu.nikhil.gupta@gmail.com>
2025-09-13 21:44:48 +0530

    Update custome.js

--- custome.js
+++ custome.js
@@ -1,7 +1,5 @@
 let input = "112334";
 //output = {1:2,2:1,3:2,4:1}
-
-
 let newobj={};
 for (var i = 0; i < input.length; i++) {
   if(!newobj[input[i]]){
@@ -10,14 +8,10 @@ for (var i = 0; i < input.length; i++) {
     newobj[input[i]]++
   }
 }
-
 console.log(newobj,'newobj')
-
-
+===================================================================================
 let input = "banana";
 //output = {1:2,2:1,3:2,4:1}
-
-
 let newobj={};
 for (var i = 0; i < input.length; i++) {
   if(!newobj[input[i]]){
@@ -26,5 +20,19 @@ for (var i = 0; i < input.length; i++) {
     newobj[input[i]]++
   }
 }
-
 console.log(newobj,'newobj')
+
+===================================================================================
+let input = "aabbaaabc";
+//output = [["a",2], ["b",2], ["a",3], ["b",1], ["c",1]]
+let newArray=[];
+let count=1;
+for (var i = 1; i <= input.length; i++) {
+  if(input[i] === input[i-1]){
+    count++;
+}else{
+   newArray.push([input[i - 1], count]);
+    count =1;
+  }
+}
+console.log(newArray)
