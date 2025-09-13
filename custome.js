07aeb9b35d5c3be0eda4234e6204b24cf31d4617
himanshuDev <himanshu.nikhil.gupta@gmail.com>
2025-09-13 21:30:31 +0530

    Create custome.js

---
+++ custome.js
@@ -0,0 +1,14 @@
+let input = "112334";
+//output = {1:2,2:1,3:2,4:1}
+
+
+let newobj={};
+for (var i = 0; i < input.length; i++) {
+  if(!newobj[input[i]]){
+    newobj[input[i]]=1
+  }else{
+    newobj[input[i]]++
+  }
+}
+
+console.log(newobj,'newobj')
