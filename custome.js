576a664f32d50fa46f9074d6b8be312cd73475c0
himanshuDev <himanshu.nikhil.gupta@gmail.com>
2025-09-13 21:31:21 +0530

    Update custome.js

--- custome.js
+++ custome.js
@@ -12,3 +12,19 @@ for (var i = 0; i < input.length; i++) {
 }
 
 console.log(newobj,'newobj')
+
+
+let input = "banana";
+//output = {1:2,2:1,3:2,4:1}
+
+
+let newobj={};
+for (var i = 0; i < input.length; i++) {
+  if(!newobj[input[i]]){
+    newobj[input[i]]=1
+  }else{
+    newobj[input[i]]++
+  }
+}
+
+console.log(newobj,'newobj')
