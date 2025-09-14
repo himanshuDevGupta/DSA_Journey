0c86e7864f012e3ef41b03d783be659fc17edf43
himanshuDev <himanshu.nikhil.gupta@gmail.com>
2025-09-14 23:00:29 +0530

    Update recursion.js

--- recursion.js
+++ recursion.js
@@ -1,3 +1,7 @@
+Recursion in JavaScript is a programming technique where a function calls itself repeatedly until a specific condition,
+known as the base case, is met. This technique is used to solve problems that can be broken down into smaller,
+similar sub-problems. 
+
 // 1
 // 2
 // 3
@@ -13,11 +17,19 @@ let n=10;
 
 function fun(x){
   if(x > n) return;
-
   console.log(x);
-
   fun(++x)
-
 }
 
 fun(1)
+----------------------------
+function sum(n){
+    if(n == 0){
+      return 0;  
+    } 
+   return n + sum(n-1);
+}
+console.log(sum(5))
+
+
+
